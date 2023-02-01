a158ac018c79d9bde66ce34395d413105a2902bc
BlingBlingJK <fjkzzcq@163.com>
2023-02-01 22:39:57 +0800

    KOF1.0

---
+++ static/js/player/kyo.js
@@ -0,0 +1,34 @@
+import { Player } from "/static/js/player/base.js";
+import { GIF } from "/static/js/utils/gif.js";
+
+export class Kyo extends Player {
+    constructor(root, info) {
+        super(root, info);
+        this.init_animations();
+    }
+
+
+    init_animations() {
+        let outer = this;
+        let offsets = [0, -22, -22, -160, 0, 0, 0];
+        for (let i = 0; i < 7; i++) {
+            let gif = GIF();
+            gif.load(`/static/images/player/kyo/${i}.gif`);
+            this.animations.set(i, {
+                gif: gif,
+                frame_cnt: 0,///总图片数
+                frame_rate: 5, //每5帧过度一次，不让人物跳的很快，不让抽搐
+                offset_y: offsets[i],//y方向偏移量
+                loaded: false,//是否加载完成
+                scale: 2.3,//放大多少倍
+            });
+
+            gif.onload = function () {
+                let obj = outer.animations.get(i);
+                obj.frame_cnt = gif.frames.length;
+                obj.loaded = true;
+            }
+        }
+    }
+}
+
